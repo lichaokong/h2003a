4f83184d2ddb424deecd65fd442712b3bc608601
李超孔 <li0123456789>
2021-01-20 21:37:17 +0800

    1.5

--- src/main/java/com/test/Test2.java
+++ src/main/java/com/test/Test2.java
@@ -3,6 +3,5 @@ package com.test;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("张三4===============");
-        System.out.println("张三4===============");
     }
 }
