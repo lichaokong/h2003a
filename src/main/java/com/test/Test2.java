3194a8b1f545aeb594afd1e74baefa373ba002ac
李超孔 <li0123456789>
2021-01-20 21:05:36 +0800

    1.2

--- src/main/java/com/test/Test2.java
+++ src/main/java/com/test/Test2.java
@@ -2,12 +2,6 @@ package com.test;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("张三2===============");
-        System.out.println("张三3===============");
-        System.out.println("张三4===============");
-        System.out.println("张三4===============");
-        System.out.println("张三4===============");
-        System.out.println("张三4===============");
         System.out.println("张三4===============");
     }
 }
